12bb37e9c823672ad583e57c3f55a008aa12e50f
MrThanlon <i@anclx.cc>
2023-10-29 18:43:44 +0800

    add en translation
    Signed-off-by: MrThanlon <i@anclx.cc>

--- conf.py
+++ conf.py
@@ -21,9 +21,11 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
 
 language = 'zh_CN'
+locale_dir = ['locale/']
+gettext_compact = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
